6f9a8f82f8a6fe57b7d771d85095d765ca2ce8dd
Sambur1 <te15.samuel.burvall@umea.nti.se>
2018-06-04 07:44:02 +0200

    Add files via upload

---
+++ Skolarbete/index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Fuz-Ion</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Navbar -->
+
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#"><img src="bilder/logo.png" class="logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+    <ul class="navbar-nav ml-auto">
+      
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Något</a>
+      </li>
+
+           <li class="nav-item">
+        <a class="nav-link" href="#">Mer</a>
+      </li>
+
+    </ul>
+  </div>
+</div>
+</nav>
+
+<!-- SLIDER -->
+<div id="slides" class="carousel slide" data-ride="carousel">
+<ul class="carousel-indicators">
+  <li data-target="#slides" data-slide-to="0" class="active"></li>
+  <li data-target="#slides" data-slide-to="1"></li>
+  <li data-target="#slides" data-slide-to="2"></li>
+</ul>
+<div class="carousel-inner">
+  
+  <div class="carousel-item active">
+    <img src="bilder/Yo2.jpg">
+    <div class="carousel-caption">
+      <h1 class="display-2">Bootstrap</h1>
+      <h3>Tack för Bootstrap</h3>
+    </div>
+  </div>
+
+  <div class="carousel-item">
+    <img src="bilder/Yo3.jpg">
+  </div>
+
+  <div class="carousel-item">
+    <img src="bilder/Yo4.jpg">
+  </div>
+
+
+</div>
+</div>
+
+<!-- FÖRSTA RUTAN -->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+  <div class="col-12">
+    <h1 class="display-4">Bild Terapi</h1>
+  </div>
+  <hr>
+  <div class="col-12">
+    <p class="lead"> Välkommen till BildterapiHuset!</p>
+  </div>
+</div>
+</div>
+
+
+</body>
+</html>
+
+<!-- ANDRA RUTAN -->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+  <div class="col-12">
+    <h1 class="display-4">Om oss...</h1>
+  </div>
+  <hr>
+  <div class="col-12">
+    <p> Dessert bonbon bear claw ice cream lollipop topping cupcake sugar plum toffee. Biscuit dragée cotton candy gummi bears candy canes. </p>
+    <p>Fruitcake macaroon chupa chups gingerbread icing cake gummies macaroon gummies. Chocolate cake powder candy canes toffee marzipan bear claw jelly-o cotton candy. Sugar plum muffin oat cake. Lollipop pudding liquorice. Sugar plum cake croissant danish chocolate bar. </p>
+    <p> Fruitcake macaroon chupa chups gingerbread icing cake gummies macaroon gummies. Chocolate cake powder candy canes toffee marzipan bear claw jelly-o cotton candy. Sugar plum muffin oat cake. Lollipop pudding liquorice. Sugar plum cake croissant danish chocolate bar. Fruitcake macaroon chupa chups gingerbread icing cake gummies macaroon gummies. Chocolate cake powder candy canes toffee marzipan bear claw jelly-o cotton candy. Sugar plum muffin oat cake. Lollipop pudding liquorice. Sugar plum cake croissant danish chocolate bar.</p>
+  </div>
+</div>
+</div>
+
+
+<!-- FIXAD BAKGRUND -->
+<figure>
+  <div class="fixed-wrap">
+    <div id="fixed">
+    </div>
+  </div>
+
+</figure>
+
+<!-- KONTAKT RUTAN -->
+<div class="container-fluid padding">
+  <div class="row welcome text-center">
+    <div class="col-12">
+    <h1 class="display-4">Kontakta oss</h1>
+    <hr>
+    <p>Fruitcake macaroon chupa chups gingerbread icing cake gummies macaroon gummies. Chocolate cake powder candy canes toffee marzipan bear claw jelly-o cotton candy. Sugar plum muffin oat cake. Lollipop pudding liquorice. Sugar plum cake croissant danish chocolate bar. </p>
+    <p> random.random@random.com </p>
+    <p> Fruitcake macaroon chupa chups gingerbread icing cake gummies macaroon gummies. Chocolate cake powder candy canes toffee marzipan bear.</p>
+    </div>
+  </div>
+  <hr class="my-4">
+</div>
+
+<!-- SOCIALA MEDIER -->
+<div class="container-fluid padding">
+<div class="row text-center padding">
+  <div class="col-12">
+    <h2>Sociala medier</h2>
+  </div>
+  <div class="col-12 social padding">
+    <a href="https://www.facebook.com/niklas.bergholm?ref=br_rs"><i class="fab fa-facebook"></i></a>
+    <a href="#"><i class="fab fa-twitter"></i></a>
+    <a href="#"><i class="fab fa-instagram"></i></a>
+    <a href="#"><i class="fab fa-youtube"></i></a>
+  </div>
+</div>
+</div>
+
+<!-- SOCIALA MEDIER -->
+<footer>
+  <div class="container-fluid padding">
+  <div class="row text-center">
+    <div class="col-12">
+    <hr class="light">
+    <h5>&copy; Bildterapihuset.se</h5>
+    </div>
+      
+  </div>
+  </div>
+</footer>
